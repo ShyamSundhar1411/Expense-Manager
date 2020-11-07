35bb6381bedccf6659867cf58d7c723375956815
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2020-11-08 01:41:27 +0530

    Project Version

--- expense/views.py
+++ expense/views.py
@@ -11,9 +11,23 @@ from django.contrib.auth import login,authenticate
 from django.http import HttpResponse,Http404
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.forms import SetPasswordForm
+from .forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 import csv
 #Class Based Views
+#Authentication
+class Signup(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
+    def form_valid(self,form):
+        v = super(Signup,self).form_valid(form)
+        username, password = form.cleaned_data.get('username'),form.cleaned_data.get('password1')
+        user = authenticate(username = username,password = password)
+        login(self.request,user)
+        return v
+
 #CRUD
 class Profile(LoginRequiredMixin,generic.UpdateView):
     model = User
